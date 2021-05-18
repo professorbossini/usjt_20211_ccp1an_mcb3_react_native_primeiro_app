2096a198ce6bd4ad31544e7f382d604ccd58c6d8
Professor Rodrigo Bossini <professorbossini@gmail.com>
2021-05-18 20:20:24 -0300

    Terminamos a apostila 02

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default class App extends React.Component{
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  constructor (props){
+    super(props);
+    this.state = {
+      contador: 0
+    }
+  }
+
+  render(){
+    return (
+      <View style={estilos.container}>
+      <Text>{this.state.contador}</Text>
+      <View style={estilos.estiloDoBotao}>
+      <Button 
+      title="Alterar texto"
+      onPress={() => this.setState({contador: this.state.contador + 1})}
+      />
+      </View>
+      </View>
+      );
+    }
+  }
+  
+  const estilos = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    estiloDoBotao: {
+      marginTop: 20
+    }
+  });
+  // export default function App() {
+    //   //hook do react
+    //   const [texto, setTexto] = useState('Texto inicial');
+    //   return (
+//     <View style={estilos.container}>
+//       <Text>{texto}</Text>
+//       <View
+//         style={estilos.estiloDoBotao}>
+//         <Button           
+//           title="Alterar texto"
+//           onPress={() => setTexto('Novo Texto')}
+//         />
+//       </View>
+//     </View>
+//   );
+// }
+
